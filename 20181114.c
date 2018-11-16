209e3bd7cf499c5cf7bc6b835598798a8ad8621b
rudyannn <39862599+rudyannn@users.noreply.github.com>
2018-11-16 13:31:56 +0900

    출력쪽에 선택기 배치하여 수정함.

--- 20181114.c
+++ 20181114.c
@@ -5,18 +5,18 @@ const int RH=LOW;
 
 const int IRinputPin=13;
 
-const int RelayPin[]={24,23,22,21,20,19,17,16,15,14};
+const int RelayPin[]={14,15,16,17,  19,20,21,22,23,24,25,26,  30,31,32,33,34,35};
 
 const int LED_Pin[]={3,4,6,7,8,10,11};
 
-const int RelayOutput_case[][10]={
-                          {RL,  RL,  RL,  RL,  RH,  RH,  RH,  RL,  RH,  RL},  
-                          {RH,  RH,  RH,  RH,  RL,  RL,  RH,  RL,  RL,  RH},  
-                          {RH,  RH,  RH,  RH,  RL,  RL,  RH,  RH,  RL,  RH},  
-                          {RL,  RL,  RL,  RL,  RL,  RL,  RL,  RH,  RL,  RL},  
-                          {RL,  RL,  RL,  RL,  RL,  RL,  RL,  RH,  RL,  RH},  
-                          {RL,  RL,  RL,  RL,  RL,  RL,  RL,  RL,  RL,  RL},
-                          {RL,  RL,  RL,  RL,  RH,  RH,  RH,  RH,  RH,  RL}  
+const int RelayOutput_case[][18]={
+    {RH, RH, RH, RH,  RH, RH, RH, RH, RH, RH, RL, RL,   RH, RH, RH, RH, RH, RH},
+    {RH, RH, RL, RL,  RH, RH, RH, RH, RH, RH, RL, RL,   RL, RL, RL, RL, RL, RL},
+    {RH, RH, RL, RL,  RH, RH, RH, RH, RH, RH, RH, RH,   RL, RL, RL, RL, RL, RL},
+    {RL, RL, RL, RL,  RL, RL, RL, RL, RL, RL, RH, RH,   RL, RL, RL, RL, RL, RL},
+    {RL, RL, RL, RL,  RH, RH, RL, RL, RL, RL, RH, RH,   RH, RH, RL, RL, RL, RL},
+    {RL, RL, RL, RL,  RL, RL, RL, RL, RL, RL, RL, RL,   RL, RL, RL, RL, RL, RL},
+    {RH, RH, RH, RH,  RH, RH, RH, RH, RH, RH, RH, RH,   RH, RH, RH, RH, RH, RH}
                           };
 
 IRrecv irrecv(IRinputPin);
@@ -26,36 +26,41 @@ IRrecv irrecv(IRinputPin);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for(int i=0; i<10; i++){
+    pinMode(IRinputPin, INPUT);
+  for(int i=0; i<19; i++){
     pinMode(RelayPin[i], OUTPUT);
     pinMode(LED_Pin[i], OUTPUT);
+    delay(50);
   }
-  
+  delay(1000);
   digitW(0);
+  delay(1000);
   
   irrecv.enableIRIn();  // Start the receiver
 }
 
 void digitW(int a)
 {
-  for(int i=0; i<10; i++)
+  for(int i=0; i<19; i++)
   {
     digitalWrite(RelayPin[i],RelayOutput_case[a][i]);
+    delay(50);
   }
   int flag[7]= {
-                RelayOutput_case[a][7],
-                RelayOutput_case[a][6],
+                RelayOutput_case[a][10],
+                RelayOutput_case[a][0],
+                RelayOutput_case[a][2],
                 RelayOutput_case[a][4],
-                RelayOutput_case[a][9] * RelayOutput_case[a][8],
-                RelayOutput_case[a][4],
-                RelayOutput_case[a][6],
-                RelayOutput_case[a][7]
+                RelayOutput_case[a][2],
+                RelayOutput_case[a][0],
+                RelayOutput_case[a][10]
                };
   
   for(int j=0; j<7; j++)
   {
     if( flag[j] == 1 )flag[j]=0;else flag[j]=1;
     digitalWrite(LED_Pin[j],flag[j]);
+    delay(50);
   }
 }
 
